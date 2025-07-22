abd0f3e0466fd9612d776ab9829128175de9af28
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-23 00:08:30 +0100

    [abbey] (UPDATE): added private variables

--- onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
+++ onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
@@ -1,9 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const ALCHEMY_PRIVATE_KEY = vars.get("ALCHEMY_PRIVATE_KEY");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  
+  networks: {
+      sepolia: {
+        url:`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_PRIVATE_KEY}`,
+        accounts: [PRIVATE_KEY]
+      }
+    },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
