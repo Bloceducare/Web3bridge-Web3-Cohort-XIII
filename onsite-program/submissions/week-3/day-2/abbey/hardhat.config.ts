fb37e8463b766411a44a4067ea1bec382ae69a86
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-23 00:36:27 +0100

    [abbey] (UPDATE): update hardhat.configs

--- onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
+++ onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
@@ -3,15 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-const ALCHEMY_PRIVATE_KEY = vars.get("ALCHEMY_PRIVATE_KEY");
+const LISK_PRIVATE_KEY = vars.get("LISK_PRIVATE_KEY");
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.30",
   networks: {
-      sepolia: {
-        url:`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_PRIVATE_KEY}`,
-        accounts: [PRIVATE_KEY]
+      lisksepolia: {
+        url:LISK_PRIVATE_KEY,
+        accounts: [`0x${PRIVATE_KEY}`]
       }
     },
   etherscan: {
