17ed211a5180ea9a85c08fd62f961f835c2e5bd8
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-23 01:14:22 +0100

    [abbey] (UPDATE): update deployed contract and verify

--- onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
+++ onsite-program/submissions/week-3/day-2/abbey/hardhat.config.ts
@@ -9,11 +9,12 @@ const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 const config: HardhatUserConfig = {
   solidity: "0.8.30",
   networks: {
-      lisksepolia: {
-        url:LISK_PRIVATE_KEY,
-        accounts: [`0x${PRIVATE_KEY}`]
-      }
-    },
+    lisk: {
+      url: LISK_PRIVATE_KEY,
+      chainId: 4202,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
   }
