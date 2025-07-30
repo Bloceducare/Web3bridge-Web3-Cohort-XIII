909e7b3c12d62631c3b56d03f198a7edaba6f92d
Martins-O <jojololamartins686@gmail.com>
2025-07-30 02:37:59 +0100

    Student management system contract completed, depolyed and verified

--- onsite-program/submissions/week-4/day-2/martins-o-jojolola/student-management-system/hardhat.config.ts
+++ onsite-program/submissions/week-4/day-2/martins-o-jojolola/student-management-system/hardhat.config.ts
@@ -1,21 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
 require("dotenv").config()
 
-const {PRIVATE_KEY, ETHERSCAN_API_KEY, LISK_SEPOLIA_URL} = process.env;
-const sep = String(ETHERSCAN_API_KEY)
+const {LISK_SEPOLIA_URL} = process.env;
+
+const ETHERSCAN_API_KEY= vars.get("ETHERSCAN_API_KEY"); 
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
 
   networks: {
-     // for testnet
     'sepolia': {
       url: LISK_SEPOLIA_URL,
       accounts: [PRIVATE_KEY as string],
       gasPrice: 1000000000,
     },
   },
+
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
   },
