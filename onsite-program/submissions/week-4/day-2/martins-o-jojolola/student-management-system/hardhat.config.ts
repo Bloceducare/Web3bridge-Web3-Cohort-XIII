a33feda03a1ed0c70c8e62145269f49d30e39ac7
Martins-O <jojololamartins686@gmail.com>
2025-07-30 03:09:44 +0100

    Student management system deployed and verified with lisk sepolia network

--- onsite-program/submissions/week-4/day-2/martins-o-jojolola/student-management-system/hardhat.config.ts
+++ onsite-program/submissions/week-4/day-2/martins-o-jojolola/student-management-system/hardhat.config.ts
@@ -12,7 +12,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
 
   networks: {
-    'sepolia': {
+    'lisk-sepolia': {
       url: LISK_SEPOLIA_URL,
       accounts: [PRIVATE_KEY as string],
       gasPrice: 1000000000,
@@ -20,7 +20,22 @@ const config: HardhatUserConfig = {
   },
 
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: {
+      "lisk-sepolia": "123"
+    },
+    customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
   },
 };
 
