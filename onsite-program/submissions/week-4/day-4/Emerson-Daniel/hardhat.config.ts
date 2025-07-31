87e11d1afcaede5c50dfdf9ecb73716809226905
Emerson majormaxx daniel <emersondaniel300@gmail.com>
2025-07-31 23:33:55 +0100

    AccessManagementSystem

--- onsite-program/submissions/week-4/day-4/Emerson-Daniel/hardhat.config.ts
+++ onsite-program/submissions/week-4/day-4/Emerson-Daniel/hardhat.config.ts
@@ -2,14 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 require('dotenv').config();
-
+const {URL_KEY, WALLET_KEY} = process.env
 const config: HardhatUserConfig = {
   solidity: "0.8.2",
   networks: {
     // for testnet
     lisk: {
-      url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [process.env.WALLET_KEY as string],
+      url: process.env.RPC_URL,
+      accounts: [`0x${WALLET_KEY}`],
       gasPrice: 1000000000,
     },
   },
