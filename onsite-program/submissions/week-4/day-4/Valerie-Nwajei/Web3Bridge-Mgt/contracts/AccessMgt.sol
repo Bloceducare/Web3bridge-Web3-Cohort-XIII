c1fce11fc79ba19d20765ea8d362cd98b9d43e63
Jayy4rl <onyinwajei@gmail.com>
2025-08-01 15:05:17 +0100

    Corrections updated

--- onsite-program/submissions/week-4/day-4/Valerie-Nwajei/Web3Bridge-Mgt/contracts/AccessMgt.sol
+++ onsite-program/submissions/week-4/day-4/Valerie-Nwajei/Web3Bridge-Mgt/contracts/AccessMgt.sol
@@ -6,14 +6,18 @@ pragma solidity ^0.8.28;
 
 contract AccessMgt{
 
-    enum Status{
-        MEMBER,
-        ADMIN
+    enum Role{
+        MediaTeam,
+        Mentors,
+        Managers,
+        SocialMediaTeam,
+        Technicians,
+        KitchenStaff
     }
     struct Employee{
         string name;
         bool isEmployed;
-        Status status;
+        Role role;
     }
 
     Employee[] public employees;
@@ -22,22 +26,50 @@ contract AccessMgt{
     mapping(address => Employee) public roles;
     mapping(address=>bool) private existingEmployee;
 
-    function addEmployee(string memory _name, Status _status, address _address) external {
+    function add_and_update_Employee(string memory _name, Role _role, address _address) external {
         require(_address != address(0), "Invalid address");
-
-        Employee memory newEmployee = Employee({
+        if(existingEmployee[_address] ==true){
+            for(uint i; i< employees.length; i++){
+            if(employees[i].isEmployed == true){
+                employees[i].name = _name;
+                employees[i].role = _role;
+            }else{
+                Employee memory newEmployee = Employee({
             name: _name,
             isEmployed: true,
-            status: _status
+            role: _role
         });
-        roles[_address]= newEmployee;
-        employees.push(newEmployee);
+            roles[_address]= newEmployee;
+            existingEmployee[_address]=true;
+            employees.push(newEmployee);
+            }
+        }
+        }
+        
+        
+    }
+    function fullAccess(address _address)external view returns(bool){
+            return (
+                existingEmployee[_address] == true &&
+                (
+                    roles[_address].role == Role.MediaTeam ||
+                    roles[_address].role == Role.Mentors ||
+                    roles[_address].role == Role.Managers
+                )
+            );        
     }
 
-    function fullAccess(address _address)external view returns(bool){
-        Employee memory emp = roles[_address];
+    function terminateEmployee(address _address) external{
+        require(_address != address(0), "Invalid address");
 
-        return emp.isEmployed && (emp.status == Status.ADMIN);
+        for(uint i; i< employees.length; i++){
+            if(existingEmployee[_address] ==true){
+                existingEmployee[_address]=false;
+                roles[_address].isEmployed = false;
+            }else{
+                revert ("Employee does not exist");
+            }
+    }
     }
 
     function getEmployees() external view returns(Employee[] memory){
