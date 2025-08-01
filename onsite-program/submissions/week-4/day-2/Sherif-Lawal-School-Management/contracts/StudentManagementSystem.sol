c7fca8b41f210485840777423631d591a93f3542
thesheriflawal <63552329+thesheriflawal@users.noreply.github.com>
2025-08-01 09:20:48 +0100

    Update StudentManagementSystem.sol

--- onsite-program/submissions/week-4/day-2/Sherif-Lawal-School-Management/contracts/StudentManagementSystem.sol
+++ onsite-program/submissions/week-4/day-2/Sherif-Lawal-School-Management/contracts/StudentManagementSystem.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
 contract StudentManagementSystem {
 
-     struct Student {
+    error INVALID_STUDENT();
+    
+    struct Student {
         string name;
         uint age;
         Status status;
@@ -24,57 +24,44 @@ contract StudentManagementSystem {
         FEMALE
     }
 
+    mapping(address => Student) student;
     uint256 uniqueId = 0;
-
-    Student [] students;
 
     function create_student(
         string memory _name, 
         uint _age,
         Gender _gender
-        ) external {
-
+    ) external {
         Student memory new_student_ = Student(
             _name,
             _age,
             Status.ACTIVE,
             _gender
         );
-
-        students.push(new_student_);
-
+        
+        student[msg.sender] = new_student_;
         uniqueId++;
     }
-
-
-        function update_student(
-            uint256 _index, 
-            string memory _new_name,
-            uint _new_age
-            ) external 
-    {
-        require(_index <= students.length, "Invalid index");
-
-        students[_index].name = _new_name;
-        students[_index].age =  _new_age;
+        
+    function update_student(
+        string memory _new_name,
+        uint _new_age
+    ) external {
+        student[msg.sender].name = _new_name;
+        student[msg.sender].age = _new_age;
     }
 
-    function update_student_status(uint256 _index, Status _new_status) external {
-        require(_index < students.length, "Invalid index");
-
-        students[_index].status = _new_status;
+    function get_student_by_address(
+        address _address
+    ) external view returns (Student memory) {
+        return student[_address];
     }
 
-    function get_students() external view returns (Student[] memory) {
-        return students;
+    function update_student_status(Status _new_status) external {
+        student[msg.sender].status = _new_status;
     }
 
-    function delete_student(uint _index) external {
-        require(_index <= students.length, "Invalid index");
-
-        students[_index] = students[students.length -1];
-        students.pop(); 
+    function delete_student() external {
+        delete student[msg.sender];
     }
-
-
 }
