a51ee612f730c9a5ae5b96527a71c8ed8b956c65
Biokes <106444765+Biokes@users.noreply.github.com>
2025-08-01 01:48:41 +0100

    add contract address

--- onsite-program/submissions/week-4/day-4/day4-task2/BABALOLA ABIODUN AYOMIDE/contracts/SchoolManagementSystem.sol
+++ onsite-program/submissions/week-4/day-4/day4-task2/BABALOLA ABIODUN AYOMIDE/contracts/SchoolManagementSystem.sol
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
 
-contract SchoolManagement {
+contract SchoolManagementSystem {
     enum Gender{MALE,FEMALE}
     enum Status { ACTIVE, DEFERRED, RUSTICATED }
     struct Student{
@@ -11,91 +12,100 @@ contract SchoolManagement {
         uint id;
         Status status;
     }
+    uint private nextStudentId = 1000;
+    error INVALID_ARGS();
+    mapping (address=> Student[]) public usersSchool;
 
-    Student[] private allStudents;
-    function registerStudent(string memory name,uint age, Gender gender) external {
+    function registerStudent( string memory name,uint age, Gender gender) external {
         require(bytes(name).length>0,"invalid student name");
         require(age> 0,"premature student registration is not allowed");
-        uint id = allStudents.length +1001;
-        allStudents.push(Student(name, age, gender, id,Status.ACTIVE));
+        uint id = nextStudentId++;
+        usersSchool[msg.sender].push(Student(name, age, gender, id,Status.ACTIVE));
     }
-    function updateStudentProfile(string memory name, uint id) public {
-        require(bytes(name).length>0,"invalid student name");
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id== id){
-                allStudents[count].name= name;
-                return;
-            }
-        }
-        revert("Student with id not found");
-    }
-    function updateStudentProfile(uint age, uint id) public {
+    function updateStudentAge(uint age, uint id) external {
         require(age > 0, "Invalid age");
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                allStudents[count].age = age;
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint i = 0; i < students.length; i++) {
+            if (students[i].id == id) {
+                students[i].age = age;
                 return;
             }
         }
-        revert("Student with id not found");
+        revert INVALID_ARGS();
     }
 
+    function updateStudentProfile(string memory name, uint id) public {
+        require(bytes(name).length>0,"invalid student name");
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if (students[count].id == id) {
+                students[count].name = name;
+                return;
+            }
+        }
+        revert INVALID_ARGS();
+    }
     function updateStudentProfile(uint age, uint id, string memory name) external{
-        updateStudentProfile(age, id);
+        this.updateStudentAge(age, id);
         updateStudentProfile(name, id);
     }
 
     function suspendStudent(uint id) external{
-
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                require(allStudents[count].status == Status.ACTIVE, "Only active students can be suspended");
-                allStudents[count].status = Status.DEFERRED;
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if(students[count].id == id){
+                require(students[count].status == Status.ACTIVE, "Only active students can be suspended");
+                students[count].status = Status.DEFERRED;
                 return;
             }
         }
-        revert("Student with id not found");
+        revert INVALID_ARGS();
     }
 
     function cancelStudentSuspension(uint id) external{
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                require(allStudents[count].status == Status.DEFERRED, "Student not suspended");
-                allStudents[count].status = Status.ACTIVE;
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if(students[count].id == id){
+                require(students[count].status == Status.DEFERRED, "Student not suspended");
+                students[count].status = Status.ACTIVE;
                 return;
             }
         }
-        revert("Student with id not found");
+        revert INVALID_ARGS();
     }
 
     function getStudentBy(uint id) external view returns (Student memory){
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                return allStudents[count];
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if(students[count].id == id){
+                return students[count];
             }
         }
-        revert("Student with id not found");
+        revert INVALID_ARGS();
     }
 
-    function getAllStudents() external view returns (Student[] memory){
-        return allStudents;
+    function getuserSchool() external view returns (Student[] memory){
+        return usersSchool[msg.sender];
     }
 
     function rusticateStudent(uint id) external {
-
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                require(allStudents[count].status != Status.RUSTICATED, "Already rusticated");
-                allStudents[count].status = Status.RUSTICATED;
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if(students[count].id == id){
+                require(students[count].status != Status.RUSTICATED, "Already rusticated");
+                students[count].status = Status.RUSTICATED;
                 return;
             }
         }
-        revert("Student with id not found");
+        revert INVALID_ARGS();
     }
     function deleteStudent(uint id)external {
-        for (uint count = 0; count < allStudents.length; count++){
-            if(allStudents[count].id == id){
-                delete allStudents[count];
+        Student[] storage students = usersSchool[msg.sender];
+        for (uint count = 0; count < students.length; count++){
+            if(students[count].id == id){
+                Student memory lastStudent = students[students.length - 1];
+                students[count] = lastStudent;
+                students.pop();
                 return;
             }
         }
