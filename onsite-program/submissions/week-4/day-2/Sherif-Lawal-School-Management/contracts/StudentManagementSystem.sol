8a9a5fd0bc34292850f47cc54aec074bdee00442
thesheriflawal <63552329+thesheriflawal@users.noreply.github.com>
2025-08-01 08:17:39 +0100

    Update StudentManagementSystem.sol

--- onsite-program/submissions/week-4/day-2/Sherif-Lawal-School-Management/contracts/StudentManagementSystem.sol
+++ onsite-program/submissions/week-4/day-2/Sherif-Lawal-School-Management/contracts/StudentManagementSystem.sol
@@ -8,11 +8,9 @@ contract StudentManagementSystem {
 
      struct Student {
         string name;
-        string email;
         uint age;
         Status status;
         Gender gender;
-        Level level;
     }
 
     enum Status {
@@ -25,82 +23,46 @@ contract StudentManagementSystem {
         MALE,
         FEMALE
     }
-    
-    enum Level {
-        SS1,
-        SS2,
-        SS3
-    }
 
-    Student [] public students;
+    uint256 uniqueId = 0;
 
-    event StudentCreated(uint indexed studentId, string name, string email, uint age, Status status, Gender gender, Level level);
-    event StudentUpdated(uint indexed studentId, string name, string email, uint age);
-    event StudentStatusUpdated(uint indexed studentId, Status newStatus);
-    event StudentGenderUpdated(uint indexed studentId, Gender newGender);
-    event StudentLevelUpdated(uint indexed studentId, Level newLevel);
-    event StudentDeleted(uint indexed studentId);
+    Student [] students;
 
     function create_student(
         string memory _name, 
-        string memory _email, 
         uint _age,
-        Status _status,
-        Gender _gender,
-        Level _level
+        Gender _gender
         ) external {
 
-        Student memory new_student_ = Student({
-            name: _name, 
-            email: _email,
-            age: _age,
-            status: _status,
-            gender: _gender,
-            level: _level
-        });
+        Student memory new_student_ = Student(
+            _name,
+            _age,
+            Status.ACTIVE,
+            _gender
+        );
 
         students.push(new_student_);
-        uint studentId = students.length -1;
 
-        emit StudentCreated(studentId, _name, _email, _age, _status, _gender, _level);
+        uniqueId++;
     }
 
 
         function update_student(
             uint256 _index, 
-            string memory _new_name, 
-            string memory _new_email, 
+            string memory _new_name,
             uint _new_age
             ) external 
     {
         require(_index <= students.length, "Invalid index");
 
         students[_index].name = _new_name;
-        students[_index].email = _new_email;
         students[_index].age =  _new_age;
-
-        emit StudentUpdated(_index, _new_name, _new_email, _new_age);
     }
 
     function update_student_status(uint256 _index, Status _new_status) external {
         require(_index < students.length, "Invalid index");
 
         students[_index].status = _new_status;
-        emit StudentStatusUpdated(_index, _new_status);
-    }
-
-    function update_student_gender(uint256 _index, Gender _new_gender) external {
-        require(_index < students.length, "Invalid index");
-
-        students[_index].gender = _new_gender;
-        emit StudentGenderUpdated(_index, _new_gender);
-    }
-
-    function upfate_student_level(uint256 _index, Level _new_level) external {
-        require(_index < students.length, "Invalid index");
-
-        students[_index].level = _new_level;
-        emit StudentLevelUpdated(_index, _new_level);
     }
 
     function get_students() external view returns (Student[] memory) {
@@ -110,12 +72,9 @@ contract StudentManagementSystem {
     function delete_student(uint _index) external {
         require(_index <= students.length, "Invalid index");
 
-        students[_index] = students[students.length -1]; // Pushes the student to the last on the list
-        students.pop(); // Deletes the last student...
-
-        emit StudentDeleted(_index);
+        students[_index] = students[students.length -1];
+        students.pop(); 
     }
-
 
 
 }
