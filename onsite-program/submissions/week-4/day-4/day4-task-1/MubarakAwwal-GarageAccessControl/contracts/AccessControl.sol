ed59c0e3dfa571626285a3207c6c3803406bcf66
fedsuites <106149540+fedsuites@users.noreply.github.com>
2025-08-02 11:16:03 +0100

    Update AccessControl.sol

--- onsite-program/submissions/week-4/day-4/day4-task-1/MubarakAwwal-GarageAccessControl/contracts/AccessControl.sol
+++ onsite-program/submissions/week-4/day-4/day4-task-1/MubarakAwwal-GarageAccessControl/contracts/AccessControl.sol
@@ -1,59 +1,74 @@
-//SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
 contract AccessControl{
     address HR;
    /* constructor(address){
-        HR=msg.sender;
+      HR=msg.sender;
     }*/
     error onlyHR();
-    modifier onlyAdmin(address){
+    modifier onlyAdmin(){
         if(msg.sender!=HR){
-            revert onlyHR();
+         revert onlyHR();
+         
         }
         _;
     }
-      
-    enum Role{
-       MediaTeam, Mentor,Manager,SocialMediaTeam,TechnicalSupervisor,KitchenStaff,Terminated
-    }
-    struct Employee{
+    enum Role{Manager,Mentor,Cleaner,KitchenStaff,Terminated}
+    struct EmployeeData{
         string name;
         Role role;
-        bool isEmployed;
     }
-    mapping(address=>Employee)public employees;
-    address[]public EmployeesList;
-    function addUpdateEmployee(address _address,string memory _name,Role _role,bool _isEmployed)external onlyAdmin(msg.sender){
-        Employee memory newEmployee=Employee(_name,_role,_isEmployed);
-        employees[_address]=newEmployee;
-        bool AlreadyEmployed;
-        for(uint i=0;i<EmployeesList.length;i++){
-            if(EmployeesList[i]==_address){
-             AlreadyEmployed=true;
-             return;
-            }  
-        }
-           if(!AlreadyEmployed){
-            EmployeesList.push(_address);
-        }
+    mapping (address=>EmployeeData) employees;
+    mapping (address=>bool) isEmployed;
+    address[] public EmployeesList;
+    function addEmployee(address _address, string memory _name, Role _role) external onlyAdmin(){
+       EmployeeData memory newEmployee=EmployeeData(_name,_role);
+       employees[_address]=newEmployee;
+       if(!isEmployed[_address]){
+        isEmployed[_address]=true;
+        EmployeesList.push(_address);
+       }
     }
-    function hasAccess(address _address)external view returns (bool){
-        Employee memory newEmployee=employees[_address];
-      if(!newEmployee.isEmployed){
-        return false;
-      }
-      
-         if(newEmployee.role==Role.Manager||newEmployee.role==Role.MediaTeam||newEmployee.role==Role.Mentor){
-            return true;
-        }
-        return false;
-      
+       function checkRole(address _address) public view returns(string memory) {
+          EmployeeData memory getData=employees[_address];
+          if(getData.role==Role.Manager)return "Manager";
+          if(getData.role==Role.Mentor)return "Mentor";
+          if(getData.role==Role.Cleaner)return "Cleaner";
+          if(getData.role==Role.KitchenStaff)return "KitchenStaff";
+          return  "Unknown/Fired";
     }
-    function getAllEmployees()external view returns(address[]memory){
-        return EmployeesList;
+    function hasAccess(address _address) external view returns(string memory){
+          EmployeeData memory getData=employees[_address];
+          if(getData.role==Role.Manager||getData.role==Role.Mentor){
+            return "This employee has access";
+          }
+          if(getData.role==Role.Cleaner||getData.role==Role.KitchenStaff)return "This employee does not have access";
+          return "This employee does not exist or has been fired";
     }
-    function getEmployee(address _address)external view returns (Employee memory){
-        return employees[_address];
+     function fireEmployee(address _address) external {
+        employees[_address].role=Role.Terminated;
+        isEmployed[_address]=false;
+       
+     }
+    function getEmployee(address _address) external view returns(string memory ,string memory ,bool){
+        EmployeeData memory getData=employees[_address];
+        return (getData.name,checkRole(_address),isEmployed[_address]);
     }
+
+ function getAllEmployee()external view returns(address[]memory){
+    return EmployeesList;
+ }   
 }
+
+
+
+
+
+
+
+
+
+
+
+
