edca8ca4fa7432845fea118c13b6033d1fdae8b1
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-08-02 05:42:44 +0100

    completed my task

--- onsite-program/submissions/week-4/day-4/day4-task2/eregha-thompson/contracts/Todo.sol
+++ onsite-program/submissions/week-4/day-4/day4-task2/eregha-thompson/contracts/Todo.sol
@@ -2,64 +2,85 @@
 pragma solidity ^0.8.28;
 
 contract TodoList {
-    struct Todo{
+    struct Todo {
+        uint UID;
         string title;
         string description;
         bool status;
         address owner;
     }
 
-    
     Todo[] private todos;
-    mapping (address => Todo[]) private todo;
+    mapping(address => Todo[]) private todo;
+    mapping(uint => uint) private IdIndex;
+    uint private nextId;
 
-    function create_todo(string memory _title, string memory _description) external {
-        
-        Todo memory new_todo_ = Todo({title: _title, status: false, description: _description, owner: msg.sender});
+    function create_todo(
+        string memory _title,
+        string memory _description
+    ) external {
+        Todo memory new_todo_ = Todo({
+            title: _title,
+            status: false,
+            description: _description,
+            owner: msg.sender,
+            UID: nextId
+        });
+        IdIndex[nextId] = todos.length - 1;
+        nextId++;
         todos.push(new_todo_);
         todo[msg.sender].push(new_todo_);
 
         // todos.push(Todos(title, description, false));
     }
-    function update_todo_by_address(address owner,uint256 index, string memory _new_title, string memory _new_description) external{
-      
-      require(owner==msg.sender, "You are not the owner");
-      require (index < todo[owner].length, "invalid index");
-            todo[owner][index].title = _new_title;
-            todo[owner][index].description = _new_description;
-       
-    }
 
-    function update_todo(uint256 _index, string memory _new_title, string memory _new_description) external{
-        require(_index <= todos.length, "Invalid index");
-        todos[_index].title = _new_title;
-        todos[_index].description = _new_description;
+    function update_todo(
+        uint256 _index,
+        string memory _new_title,
+        string memory _new_description
+    ) external {
+        uint newIndex = IdIndex[_index];
+        require(todos[newIndex].owner == msg.sender, "invalid owner");
+        todos[newIndex].title = _new_title;
+        todos[newIndex].description = _new_description;
+
+        Todo[] storage userTodos = todo[msg.sender];
+        for (uint256 i = 0; i < userTodos.length; i++) {
+            if (userTodos[i].UID == _index) {
+                userTodos[i].title = _new_title;
+                userTodos[i].description = _new_description;
+            }
+        }
     }
 
     function toggle_todo_status(uint256 _index) external {
-      
-      todos[_index].status = !todos[_index].status;
+        todos[_index].status = !todos[_index].status;
     }
 
-    function get_todos() external view returns (Todo[] memory){
+    function get_todos() external view returns (Todo[] memory) {
         return todos;
     }
-    function get_todos_by_address() external view returns (Todo[] memory){
-        
-            return todo[msg.sender];
+    function get_todos_by_address() external view returns (Todo[] memory) {
+        return todo[msg.sender];
     }
 
-//     function delete_todo(uint256 _index) external {
-//         require(_index <= todos.length, "Invalid index");
-//         delete todos[_index];       
-// }
-function delete_todo( uint index) external  {
-    require(index < todo[msg.sender].length, "invalid index");
-    require(msg.sender == todo[msg.sender][index].owner);
-    
-    todo[msg.sender][index] = todo[msg.sender][todo[msg.sender].length-1];
-    todo[msg.sender].pop();
+    //     function delete_todo(uint256 _index) external {
+    //         require(_index <= todos.length, "Invalid index");
+    //         delete todos[_index];
+    // }
+    function delete_todo(uint _index) external {
+        require(_index < todos.length, "invalid index");
+        require(todos[_index].owner == msg.sender, "invalid owner");
 
- }
+        todos[_index] = todos[todos.length - 1];
+        todos.pop();
 
+        Todo[] storage userTodos = todo[msg.sender];
+        for (uint256 i = 0; i < userTodos.length; i++) {
+            if (userTodos[i].UID == _index) {
+                userTodos[i] = userTodos[userTodos.length - 1];
+                userTodos.pop();
+            }
+        }
+    }
 }
