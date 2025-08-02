76443b7015f26c33094445d289d2d47aa6a0a881
josiah-praise <praisechinonso21@gmail.com>
2025-08-02 13:23:13 +0100

    final commit

--- onsite-program/submissions/week-4/day-6/day-6-task-1-Josiah-Praise/hardhat.config.ts
+++ onsite-program/submissions/week-4/day-6/day-6-task-1-Josiah-Praise/hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import * as dotenv from "dotenv";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    liskSepolia: {
+      url: "https://rpc.sepolia-api.lisk.com",
+      chainId: 4202,
+      accounts: [process.env.PRIVATE_KEY!],
+      gasPrice: "auto",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      liskSepolia: "abc", // Dummy key for Lisk Sepolia
+    },
+    customChains: [
+      {
+        network: "liskSepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
---
+++ onsite-program/submissions/week-4/day-6/day-6-task-1-Josiah-Praise/ignition/modules/EMS.ts
@@ -0,0 +1,15 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const EMSModule = buildModule("EMSModule", (m) => {
+
+  const ems = m.contract("EMS");
+
+  return { ems };
+});
+
+export default EMSModule;
