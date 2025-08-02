8966e4d5f5a7dfe1381d9ce1835b76ecaf16ec8b
Mosas <tosinolosasa@gmail.com>
2025-08-02 12:45:11 +0100

    Add school management mapping implementation

--- onsite-program/submissions/week-4/day-4/Adebayo-Muhammed/TodoMapping/ignition/modules/Schoolmanagement.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const SchoolManagementSystemModule = buildModule("SchoolManagementSystemModule", (m) => {
-  const schoolManagementSystem = m.contract("SchoolManagementSystem");
-
-  return { schoolManagementSystem };
-});
-
-export default SchoolManagementSystemModule;
