07456e24b92cda44b6bedc6ba3b808c90f0888eb
oderahub <okere_chidera@yahoo.com>
2025-08-02 13:16:19 +0100

    fix

--- onsite-program/submissions/week-4/day-6/day-6-task-1/chidera-okere/ignition/modules/Employee.ts
+++ onsite-program/submissions/week-4/day-6/day-6-task-1/chidera-okere/ignition/modules/Employee.ts
@@ -1,20 +1,13 @@
 // This setup uses Hardhat Ignition to manage smart contract deployments.
 // Learn more about it at https://hardhat.org/ignition
 
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
+const EmployeeModule = buildModule('EmployeeModule', (m) => {
+  // Deploy the Employee contract
+  const employee = m.contract('EmployeeWithSalary')
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+  return { employee }
+})
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
-});
-
-export default LockModule;
+export default EmployeeModule
