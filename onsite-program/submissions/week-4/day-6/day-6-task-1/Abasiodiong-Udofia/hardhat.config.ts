db6058eef25b59a38b738ca4f77938468f808cb4
Odiong <enalsy22@gmail.com>
2025-08-02 13:09:43 +0100

    feat(employees): Employee management system

---
+++ onsite-program/submissions/week-4/day-6/day-6-task-1/Abasiodiong-Udofia/hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY as string;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
