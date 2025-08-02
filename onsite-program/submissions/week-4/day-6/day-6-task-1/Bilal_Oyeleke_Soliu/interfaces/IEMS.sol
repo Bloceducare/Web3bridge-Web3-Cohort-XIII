e0c1f9680ca7149e4f75a523e09fa4eaffc579c6
Sobilo34 <bilalsolih60@gmail.com>
2025-08-02 13:22:48 +0100

    Bilal

---
+++ onsite-program/submissions/week-4/day-6/day-6-task-1/Bilal_Oyeleke_Soliu/interfaces/IEMS.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+interface IEMS {
+    enum EmploymentStatus {
+        Employed,
+        NotEmployed,
+        OnProbation
+    }
+
+    struct Role {
+        string mentor;
+        string admin;
+        string security;
+    }
+
+    struct Employee {
+        address userAddress;
+        uint balance;
+        uint salary;
+        Role role;
+        EmploymentStatus status;
+    }
+
+    function registerUser(uint _salary, Role memory _role) external;
+    function getBalance(address _address) external view returns (uint);
+    function paySalary(address payable _employee_address) external;
+    function getEmployees() external view returns (Employee[] memory);
+    function getAnEmployee(address _userAddress) external view returns (Employee memory);
+}
