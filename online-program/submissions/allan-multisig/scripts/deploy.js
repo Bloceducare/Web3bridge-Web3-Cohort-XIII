8b8847d71fad9a1a6363baed696ce2aec33b1be1
Githaiga22 <allangithaiga5@gmail.com>
2025-08-04 20:07:59 +0300

    add multisig addresses for 3

--- online-program/submissions/allan-multisig/scripts/deploy.js
+++ online-program/submissions/allan-multisig/scripts/deploy.js
@@ -1,18 +1,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  const [deployer, owner1, owner2, owner3] = await hre.ethers.getSigners();
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying from:", deployer.address);
 
   const MultiSigWallet = await hre.ethers.getContractFactory("MultiSigWallet");
+  const multisig = await MultiSigWallet.deploy(
+    ["0xAA6E05A031f9EE46311A61d3C65a646f7392b4fa", "0x2eC6be1A02b8697367f95082e94c3789b917E22A", "0x4B359D10c05b8615B437AA76bD4A5aEef4FdDE9F"], // Replace with actual owner addresses
+    2 // confirmationsRequired
+  );
 
-  const owners = [owner1.address, owner2.address, owner3.address];
-  const requiredConfirmations = 2;
+  await multisig.waitForDeployment();
 
-  const wallet = await MultiSigWallet.deploy(owners, requiredConfirmations);
-
-  await wallet.waitForDeployment();
-
-  console.log("MultiSigWallet deployed to:", await wallet.getAddress());
+  console.log("MultiSigWallet deployed to:", await multisig.getAddress());
 }
 
 main().catch((error) => {
