6f85ebef5a96059662cf25477d384f0bc0d0afb9
Sobilo34 <bilalsolih60@gmail.com>
2025-08-04 02:10:43 +0100

    ass: Implement ERC20 (Exchangable/Fungible) Token Standard - Use the Concepts taught in the Last week class. Including: - mappings - constructor - Interfaces - Inheritance - Libraries And additional ones like: - Events

---
+++ onsite-program/submissions/week-5/day-1/Bilal_Oyeleke_Soliu/contracts/ERC20.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "../interfaces/IERC20.sol";
+import "../libraries/LERC20.sol";
+
+contract ERC20Token is IERC20 {
+    string name;
+    string symbol;
+    uint decimal;
+    address owner;
+    uint public totalSupply;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        name = "BilalERC20Token";
+        symbol = "BETK";
+        decimal = 18;
+
+        owner = msg.sender;
+    }
+
+    using LERC20 for * ;
+
+    function mint(address _to, uint _amount) public {
+        if (_to != msg.sender) {
+            revert LERC20.ONLY_OWNER_CAN_CALL_THIS_FUNCTION();
+        }
+        totalSupply += _amount;
+        balances[_to] += _amount;
+        emit Transfer(address(0), _to, _amount); 
+    }
+
+    function burn(uint _amount) public {
+        if (owner != msg.sender) {
+            revert LERC20.ONLY_OWNER_CAN_CALL_THIS_FUNCTION();
+        }
+        totalSupply -= _amount;
+        balances[owner] -= _amount;
+        emit Transfer(owner, address(0), _amount); 
+    }
+
+    function _totalSupply() view external returns (uint) {
+        return totalSupply;
+    }
+
+    function balanceOf(address account) external view returns(uint) {
+        return balances[account];
+    }
+
+    function transfer(address _to, uint _amount) external returns (bool) {
+        if (balances[msg.sender] <= _amount) {
+            revert LERC20.NOT_ENOUGH_FUND();
+        }
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+
+        return true;
+    }
+
+    mapping (address => mapping(address => uint)) allowances;
+
+    function approve(address _spender, uint _amount) external returns(bool) {
+        allowances[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint) {
+        return allowances[_owner][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint _amount) public returns (bool) {
+        if (balances[_from] <= _amount) {
+            revert LERC20.NOT_ENOUGH_FUND();
+        }
+
+        if (allowances[_from][msg.sender] <= _amount) {
+            revert LERC20.ALLOWANCE_TOO_LOW();
+        }
+
+        balances[_from] -= _amount;
+        balances[_to] += _amount;
+        allowances[_from][msg.sender] -= _amount;
+
+        emit Transfer(_from, _to, _amount);
+
+        return true;
+    }
+}
