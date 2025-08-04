b555422f31b51f328ea69f6e2e899d97c86ef7fe
IyanuOluwaJesuloba <jesulobaowoseni1@gmail.com>
2025-08-04 01:08:24 +0100

    Update

--- onsite-program/submissions/week-5/day-1/Iyanuoluwa/contracts/Project.sol
+++ onsite-program/submissions/week-5/day-1/Iyanuoluwa/contracts/Project.sol
@@ -1,17 +1,26 @@
 //SPDX -License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+error InsufficientBalance();
+error InvalidSender();
+error InvalidReceiver();
+// error InsufficientAllowance;
+// error InvalidApprover;
+error InvalidSpender();
+
 interface IERC20 {
     function transfer ( address recipient, uint amount) external returns (bool);
      function transferFrom (address sender, address recipient, uint amount) external returns (bool);
     function approve ( address spender, uint amount) external returns (bool);
-    // function allowance (address sender, address spender) external returns (uint)
+    function totalSupply () external view returns (uint);
+    function balanceOf (address account) external view returns (uint);
+    function allowance (address sender, address spender) external returns (uint);
 }
 
 contract ERC20 is IERC20 {
-    uint public totalSupply;
-    mapping (address=>uint) public balanceOf;
-    mapping(address=> mapping(address=>uint)) public allowance;
+    uint private _totalSupply;
+    mapping (address=>uint) public _balance;
+    mapping(address=> mapping(address=>uint)) public _allowance;
     string public name ="cakeToken";
     string public symbol = "CAKE";
     uint public decimal = 18;
@@ -19,23 +28,58 @@ contract ERC20 is IERC20 {
     event Transfer(address indexed from, address indexed to, uint amount);
     event Approve(address indexed owner, address indexed spender, uint amount);
 
+    function totalSupply() external view returns (uint){
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint){
+        return _balance[account];
+    }
+
     function transfer (address recipient, uint amount) external returns (bool){
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
+        if (recipient == address(0)) {
+            revert InvalidReceiver();
+        }
+
+        if (_balance[msg.sender] < amount) {
+            revert InsufficientBalance();
+        }
+
+        _balance[msg.sender] -= amount;
+        _balance[recipient] += amount;
         emit Transfer (msg.sender, recipient, amount);
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint amount) external returns (bool){
-        balanceOf[sender] -= amount;
-        balanceOf [recipient] += amount;
-        allowance [sender][msg.sender] -=amount;
+       
+       if(sender == address(0)){
+        revert InvalidSender();
+       }
+       if(sender == address(0)){
+        revert InvalidReceiver();
+       }
+       if (_balance[sender] < amount) {
+            revert InsufficientBalance();
+       }
+
+        _balance[sender] -= amount;
+        _balance[recipient] += amount;
+        _allowance[sender][msg.sender] -= amount;
         emit Transfer (sender, recipient, amount);
         return true;
     }
 
+    function allowance (address sender, address spender) external returns (uint){
+       return _allowance[sender][spender];
+    }
+
     function approve (address spender, uint amount) external returns (bool){
-        allowance [msg.sender][spender] = amount;
+       if (spender == address(0)) {
+        revert InvalidSpender();
+       }
+
+        _allowance [msg.sender][spender] = amount;
         emit Approve (msg.sender, spender, amount);
         return true;
     }
