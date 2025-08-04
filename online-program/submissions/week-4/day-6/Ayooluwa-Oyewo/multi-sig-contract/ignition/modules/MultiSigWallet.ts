f4a35c633bc68e92d42a21499ac9c1eb6447df1a
oyewoas <ayooluwaoyewo@gmail.com>
2025-08-04 18:31:46 +0100

    multi-sig wallet contract and test

--- online-program/submissions/week-4/day-6/Ayooluwa-Oyewo/multi-sig-contract/ignition/modules/MultiSigWallet.ts
+++ online-program/submissions/week-4/day-6/Ayooluwa-Oyewo/multi-sig-contract/ignition/modules/MultiSigWallet.ts
@@ -4,9 +4,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const MultiSigWalletModule = buildModule("MultiSigWalletModule", (m) => {
-    const requiredConfirmations = m.getParameter("requiredConfirmations", 1);
+    const requiredConfirmations = m.getParameter("requiredConfirmations", 2);
     const owners = m.getParameter("owners", [
         "0x5A232799eb8BBB999B1f056A0Ba0d8C7d5c1ebe0",
+        "0x4E4E7e8ea62E6CD3568Eb218B38A8078EB83eD63",
     ]);
 
     const multiSigWallet = m.contract("MultiSigWallet", [requiredConfirmations, owners]);
