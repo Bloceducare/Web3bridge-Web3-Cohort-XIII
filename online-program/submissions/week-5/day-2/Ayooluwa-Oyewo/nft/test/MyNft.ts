b6fd5f0f756daa41fc95d9eeb6c38578a38b487e
oyewoas <ayooluwaoyewo@gmail.com>
2025-08-05 18:16:32 +0100

    mynft contract

---
+++ online-program/submissions/week-5/day-2/Ayooluwa-Oyewo/nft/test/MyNft.ts
@@ -0,0 +1,63 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("MyNft", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployMyNftFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+
+    const MyNft = await hre.ethers.getContractFactory("MyNft");
+    const myNft = await MyNft.deploy();
+
+    return { myNft, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should deploy the contract", async function () {
+      const { myNft, owner } = await loadFixture(deployMyNftFixture);
+
+      expect(await myNft.name()).to.equal("MyNft");
+      expect(await myNft.symbol()).to.equal("MNFT");
+      expect(await myNft.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Minting", function () {
+    it("Should mint an NFT", async function () {
+      const { myNft, owner } = await loadFixture(deployMyNftFixture);
+      const tokenId = await myNft.getNextTokenId();
+      expect(tokenId).to.equal(0);
+      const tokenURI = "https://example.com/token/1";
+      const tx = await myNft.mintNft(owner.address, tokenURI);
+      await expect(tx)
+        .to.emit(myNft, "NftMinted")
+        .withArgs(0, owner.address, tokenURI);
+      expect(await myNft.ownerOf(tokenId)).to.equal(owner.address);
+      expect(await myNft.tokenURI(tokenId)).to.equal(tokenURI);
+      expect(await myNft.getNextTokenId()).to.equal(1);
+    });
+
+    it("Should revert if the tokenURI is the empty string", async function () {
+      const { myNft, owner } = await loadFixture(deployMyNftFixture);
+      const tokenURI = "";
+      await expect(
+        myNft.mintNft(owner.address, tokenURI)
+      ).to.be.revertedWithCustomError(myNft, "MyNft_NoTokenURI");
+    });
+    it("Should revert if the recipient is the zero address", async function () {
+      const { myNft } = await loadFixture(deployMyNftFixture);
+      const tokenURI = "https://example.com/token/1";
+      await expect(
+        myNft.mintNft(hre.ethers.ZeroAddress, tokenURI)
+      ).to.be.revertedWithCustomError(myNft, "MyNft_NoRecipient");
+    });
+  });
+});
