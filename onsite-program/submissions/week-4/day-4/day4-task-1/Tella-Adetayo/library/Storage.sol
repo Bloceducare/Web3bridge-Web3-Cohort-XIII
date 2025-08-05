17886b9f2390fce63b5a937de50f3b464e2fb484
detayotella <tellaadetayo@gmail.com>
2025-08-05 12:39:23 +0100

    test: add unit test to employee management

---
+++ onsite-program/submissions/week-4/day-4/day4-task-1/Tella-Adetayo/library/Storage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+library Storage {
+
+    struct Employee {
+        string name;
+        bool isEmployed;
+        Role role; 
+        uint256 salary;
+        uint256 paidSoFar; 
+    }
+
+    enum Role {
+        MENTOR, 
+        ADMIN, 
+        SECURITY 
+    }
+
+    struct Layout {
+        mapping(address => Employee) employee;
+        address[] users;
+    }
+
+    bytes32 internal constant STORAGE_SLOT = keccak256("employee.storage");
+
+    function layout() internal pure returns (Layout storage ds) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            ds.slot := slot
+        }
+    }
+}
