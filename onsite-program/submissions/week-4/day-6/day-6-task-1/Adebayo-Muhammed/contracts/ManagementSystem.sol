16f45812d39d6727ea59d7a1b4e297ef7d4cdcfe
Mosas <tosinolosasa@gmail.com>
2025-08-05 13:16:19 +0100

    Management test

--- onsite-program/submissions/week-4/day-6/day-6-task-1/Adebayo-Muhammed/contracts/ManagementSystem.sol
+++ onsite-program/submissions/week-4/day-6/day-6-task-1/Adebayo-Muhammed/contracts/ManagementSystem.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "./ImanagementSysten.sol";
+import "../contracts/IManagementSystem.sol";
 
 contract ManagementSystem is IManagementSystem {
     address public owner;
