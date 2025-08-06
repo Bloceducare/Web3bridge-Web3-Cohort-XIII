dbdca80dd220320b9bc1ce63a8e0f8509216242f
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-08-06 21:25:45 +0100

    Feat: Updated

--- onsite-program/submissions/week-5/day-3/NFT-1/Akanimoh-Johnson/contracts/NFT.sol
+++ onsite-program/submissions/week-5/day-3/NFT-1/Akanimoh-Johnson/contracts/NFT.sol
@@ -1,167 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IERC721Receiver {
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external returns (bytes4);
-}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT {
-    string private _name = "Akan_Nigeria";
-    string private _symbol = "AKN";
-    string private _baseURI;
-    mapping(uint256 => address) private _owners;
-    mapping(address => uint256) private _balances;
-    mapping(uint256 => address) private _tokenApprovals;
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-    mapping(uint256 => string) private _tokenURIs;
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+contract TomiNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
 
-    constructor(string memory baseURI_) {
-        _baseURI = baseURI_;
-    }
+    event NFTMinted(address indexed recipient, uint256 indexed tokenId, string tokenURI);
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
+    constructor(address initialOwner) ERC721("TomiNFT", "TNFT") Ownable(initialOwner) {}
 
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        require(recipient != address(0), "Recipient cannot be zero address");
+        require(bytes(tokenURI).length > 0, "Token URI cannot be empty");
 
-    function _baseTokenURI() internal view returns (string memory) {
-        return _baseURI;
-    }
+        _tokenIds = 1; // Set to 1 for single NFT
+        uint256 newItemId = _tokenIds;
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
-        require(_exists(tokenId), "ERC721: Query for nonexistent token");
-        string memory uri = _tokenURIs[tokenId];
-        if (bytes(uri).length == 0) {
-            return string(abi.encodePacked(_baseURI, uint2str(tokenId), ".json"));
-        }
-        return uri;
-    }
-
-    function uint2str(uint256 _i) internal pure returns (string memory) {
-        if (_i == 0) return "0";
-        uint256 j = _i;
-        uint256 length;
-        while (j != 0) {
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint256 k = length;
-        j = _i;
-        while (j != 0) {
-            bstr[--k] = bytes1(uint8(48 + j % 10));
-            j /= 10;
-        }
-        return string(bstr);
-    }
-
-    function balanceOf(address owner) public view returns (uint256) {
-        require(owner != address(0), "ERC721: Balance query for the zero address");
-        return _balances[owner];
-    }
-
-    function ownerOf(uint256 tokenId) public view returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "ERC721: Owner query for nonexistent token");
-        return owner;
-    }
-
-    function approve(address to, uint256 tokenId) public {
-        address owner = ownerOf(tokenId);
-        require(to != owner, "ERC721: Approval to current owner");
-        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), 
-                "ERC721: Approve caller is not owner nor approved for all");
-        _tokenApprovals[tokenId] = to;
-        emit Approval(owner, to, tokenId);
-    }
-
-    function getApproved(uint256 tokenId) public view returns (address) {
-        require(_exists(tokenId), "ERC721: Approved query for nonexistent token");
-        return _tokenApprovals[tokenId];
-    }
-
-    function setApprovalForAll(address operator, bool approved) public {
-        require(operator != msg.sender, "ERC721: Approve to caller");
-        _operatorApprovals[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
-    }
-
-    function isApprovedForAll(address owner, address operator) public view returns (bool) {
-        return _operatorApprovals[owner][operator];
-    }
-
-    function transferFrom(address from, address to, uint256 tokenId) public {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721: Transfer caller is not owner nor approved");
-        _transfer(from, to, tokenId);
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId) public {
-        safeTransferFrom(from, to, tokenId, "");
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721: Transfer caller is not owner nor approved");
-        _safeTransfer(from, to, tokenId, data);
-    }
-
-    function _exists(uint256 tokenId) internal view returns (bool) {
-        return _owners[tokenId] != address(0);
-    }
-
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
-        require(_exists(tokenId), "ERC721: Operator query for nonexistent token");
-        address owner = ownerOf(tokenId);
-        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
-    }
-
-    function _transfer(address from, address to, uint256 tokenId) internal {
-        require(ownerOf(tokenId) == from, "ERC721: Transfer of token that is not owned");
-        require(to != address(0), "ERC721: Transfer to the zero address");
-        _tokenApprovals[tokenId] = address(0);
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-        emit Transfer(from, to, tokenId);
-    }
-
-    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal {
-        _transfer(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, data), "ERC721: Transfer to non ERC721Receiver");
-    }
-
-    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory data) 
-        private returns (bool) {
-        if (to.code.length > 0) {
-            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, data) returns (bytes4 retval) {
-                return retval == IERC721Receiver.onERC721Received.selector;
-            } catch {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function _mint(address to, uint256 tokenId, string memory uri) internal {
-        require(to != address(0), "ERC721: Mint to the zero address");
-        require(!_exists(tokenId), "ERC721: Token already minted");
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-        _tokenURIs[tokenId] = uri;
-        emit Transfer(address(0), to, tokenId);
-    }
-
-    function mint(address to, uint256 tokenId, string memory uri) public {
-        _mint(to, tokenId, uri);
+        emit NFTMinted(recipient, newItemId, tokenURI);
+        return newItemId;
     }
 }
--- onsite-program/submissions/week-5/day-3/NFT-1/Akanimoh-Johnson/scripts/deploy.js
+++ onsite-program/submissions/week-5/day-3/NFT-1/Akanimoh-Johnson/scripts/deploy.js
@@ -1,15 +1,32 @@
-const { ethers } = require('hardhat');
+const { ethers } = require("hardhat");
+// import { ethers } from "hardhat";
 
 async function main() {
-    const baseURI = 'https://purple-official-muskox-784.mypinata.cloud/ipfs/bafkreibd2eo2wu5evrdumfezlw7spy7gc3ib4t5jwthxo5l527opzqaoja'; 
-    const MyNFT = await ethers.getContractFactory('MyNFT');
-    const myNFT = await MyNFT.deploy(baseURI);
-    console.log('MyNFT deployed to:', myNFT.address);
+  // Replace with your Pinata CID for the NFT metadata
+  const tokenURI = "ipfs://bafkreibd2eo2wu5evrdumfezlw7spy7gc3ib4t5jwthxo5l527opzqaoja"; // e.g., ipfs://Qm...xyz
+  // Replace with the recipient address for the NFT
+  const recipient = "0x58C25c26666B31241C67Cf7B9a82e325eB07c342"; // e.g., 0x123...abc
+
+  // Get the deployer's address
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with account:", deployer.address);
+
+  // Deploy the TomiNFT contract
+  const TomiNFT = await ethers.getContractFactory("TomiNFT");
+  const tomiNFT = await TomiNFT.deploy(deployer.address);
+  await tomiNFT.waitForDeployment();
+  console.log("TomiNFT deployed to:", await tomiNFT.getAddress());
+
+  // Mint one NFT
+  const tx = await tomiNFT.mintNFT(recipient, tokenURI);
+  const receipt = await tx.wait();
+  console.log("NFT minted with token ID 1 to:", recipient);
+  console.log("Transaction hash:", receipt.hash);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
