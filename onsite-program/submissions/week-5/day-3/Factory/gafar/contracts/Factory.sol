7494a81bfdcf846e0f1d5391c4abe14c865cc68f
gafaradetunji <gafaradetunji471@gmail.com>
2025-08-07 11:51:43 +0100

    updated factory

--- onsite-program/submissions/week-5/day-3/Factory/gafar/contracts/Factory.sol
+++ onsite-program/submissions/week-5/day-3/Factory/gafar/contracts/Factory.sol
@@ -10,48 +10,4 @@ contract Factory {
     Erc20 token = new Erc20(name_, symbol_, decimals_, initialSupply);
     tokenStandards.push(token);
   }
-
-  function getName(uint256 _tokenIndex) external view returns (string memory) {
-    return Erc20(address(tokenStandards[_tokenIndex])).name();
-  }
-
-  function getSymbol(uint256 _tokenIndex) external view returns (string memory) {
-    return Erc20(address(tokenStandards[_tokenIndex])).symbol();
-  }
-
-  function getDecimal(uint256 _tokenIndex) external view returns (uint8) {
-    return Erc20(address(tokenStandards[_tokenIndex])).decimals();
-  }
-
-  function getTotalSupply(uint256 _tokenIndex) external view returns (uint256) {
-    return Erc20(address(tokenStandards[_tokenIndex])).totalSupply();
-  }
-
-  function getBalanceOf(uint256 _tokenIndex, address account) external view returns (uint256) {
-    return Erc20(address(tokenStandards[_tokenIndex])).balanceOf(account);
-  }
-
-  function transferFunc(uint256 _tokenIndex, address recipient, uint256 amount) external returns (bool) {
-    return Erc20(address(tokenStandards[_tokenIndex])).transfer(recipient, amount);
-  }
-
-  function approveFunc(uint256 _tokenIndex, address spender, uint256 amount) external returns (bool) {
-    return Erc20(address(tokenStandards[_tokenIndex])).approve(spender, amount);
-  }
-
-  function transferFromFunc(uint256 _tokenIndex, address sender, address recipient, uint256 amount) external returns (bool) {
-    return Erc20(address(tokenStandards[_tokenIndex])).transferFrom(sender, recipient, amount);
-  }
-
-  function allowanceFunc(uint256 _tokenIndex, address _owner, address _spender) view external returns (uint256) {
-    return Erc20(address(tokenStandards[_tokenIndex])).allowance(_owner, _spender);
-  }
-
-  function mintFunc(uint256 _tokenIndex, address to, uint256 amount) external {
-    return Erc20(address(tokenStandards[_tokenIndex]))._mint(to, amount);
-  }
-
-  function burnFunc(uint256 _tokenIndex, address from, uint256 amount) external {
-    return Erc20(address(tokenStandards[_tokenIndex]))._burn(from, amount);
-  }
 }
