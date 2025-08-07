f3930d5393c39d3d3d69a4f92bae3d45ca78ad18
fedsuites <106149540+fedsuites@users.noreply.github.com>
2025-08-07 08:37:57 +0100

    Update IERC20.sol

--- onsite-program/submissions/week-4/day-6/MubarakAwwalERC20/contracts/IERC20.sol
+++ onsite-program/submissions/week-4/day-6/MubarakAwwalERC20/contracts/IERC20.sol
@@ -3,10 +3,13 @@ pragma solidity 0.8.30;
 
 interface IERC20 {
 
-    function transfer(address recipient,uint amount)external returns (bool);
-    function approve(address spender,uint amount)external returns (bool);
-    function transferFrom(address sender,address recipient,uint amount)external returns(bool);
-    event Transfer(address indexed from,address indexed to, uint amount);
-    event Approval(address indexed owner, address indexed spender,uint amount);
+       function transfer(address recipient, uint amount)external returns(bool);
+       function approve(address spender,uint amount)external returns(bool);
+       function allowance(address owner,address spender) external  returns(uint);
+       function balanceOf(address _address)external  returns(uint);
+       function transferFrom(address sender,address recipient, uint amount) external returns (bool);
+       function totalSupply()external view returns(uint);
+       event Transfer(address indexed sender, address indexed recipient , uint amount);
+       event Approval(address indexed owner, address indexed  recipient, uint amount);
 
 }
