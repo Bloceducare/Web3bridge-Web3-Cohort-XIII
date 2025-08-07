c0dedccf68119953b1aa29258f48e19b3bd523c2
s-olagoke <olagokesikiru06@gmail.com>
2025-08-07 15:41:10 +0100

    chores: Updated Factory contract

--- onsite-program/submissions/week-4/day-6/day-6-task-1/Sikiru Olagoke/contracts/EmployeeFactory.sol
+++ onsite-program/submissions/week-4/day-6/day-6-task-1/Sikiru Olagoke/contracts/EmployeeFactory.sol
@@ -19,18 +19,6 @@ contract EmployeeFactory {
     
   }
 
-  function manager_pay_employee(uint256 _index, address _employer_address, uint256 _amount) external {
-        require(manager == msg.sender, "You can't pay employees");
-        EmployeeManagement employee = new EmployeeManagement(employees[_index]);
-        employee.pay_employee(_employer_address, _amount);
-
-  }
-
-  function manager_get_employees(uint256 _index) external {
-    EmployeeManagement employee = new EmployeeManagement(employees[_index]);
-    employee.get_all_employees();
-  }
-
   function get_factory_addresses() external view returns (address[] memory) {
     return employees;
   }
