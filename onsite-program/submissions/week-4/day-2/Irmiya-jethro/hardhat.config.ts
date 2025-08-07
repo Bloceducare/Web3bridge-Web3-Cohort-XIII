8d7b4a9f814566a6fa8cca07b451039a99397bea
jayteemoney <jethroirmiya@gmail.com>
2025-08-07 22:29:28 +0100

    StudentFactory task

--- onsite-program/submissions/week-4/day-2/Irmiya-jethro/hardhat.config.ts
+++ onsite-program/submissions/week-4/day-2/Irmiya-jethro/hardhat.config.ts
@@ -1,18 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
 
-require('dotenv').config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.30",
   networks: {
-    
     'lisk-sepolia': {
       url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [process.env.WALLET_KEY as string],
-      gasPrice: 1000000000,
+     
+      accounts: [vars.get("PRIVATE_KEY")],
     },
   },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123"
+    },
+    customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      }
+    ]
+  },
+ sourcify: {
+    enabled: false
+  },
 };
-
-export default config;
