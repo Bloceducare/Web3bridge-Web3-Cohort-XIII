9aaf0d7b77969b2e7d6627d105c5df9489d3af9d
detayotella <tellaadetayo@gmail.com>
2025-08-08 13:25:59 +0100

    feat: update the config.ts

--- onsite-program/submissions/week-5/day-4/Tella-Adetayo/hardhat.config.ts
+++ onsite-program/submissions/week-5/day-4/Tella-Adetayo/hardhat.config.ts
@@ -2,34 +2,34 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
-const PRIVATE_KEY = vars.get("PRIVATE_KEY"); 
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.30",
   networks: {
     "lisk-sepolia": {
-      url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [PRIVATE_KEY]
-    } 
-  }, 
+      url: "https://rpc.sepolia-api.lisk.com",
+      accounts: [PRIVATE_KEY],
+    },
+  },
   etherscan: {
     apiKey: {
-      "lisk-sepolia": "123", 
-    }, 
+      "lisk-sepolia": "123",
+    },
     customChains: [
       {
-        network: "lisk-sepolia", 
-        chainId: 4202, 
+        network: "lisk-sepolia",
+        chainId: 4202,
         urls: {
           apiURL: "https://sepolia-blockscout.lisk.com/api",
-          browserURL: "https://sepolia-blockscout.lisk.com"
-        }
-      }
-
-    ]
-
-  }, 
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
+  },
   sourcify: {
-    enabled: false
-  }
+    enabled: false,
+  },
 };
+
+export default config;
