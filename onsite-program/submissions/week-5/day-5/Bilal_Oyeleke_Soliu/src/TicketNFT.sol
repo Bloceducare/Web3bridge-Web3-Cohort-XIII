55d3a7f5fb4e17e5e0a1eed38391ad7c42fa3756
Sobilo34 <bilalsolih60@gmail.com>
2025-08-09 02:15:59 +0100

    feat: Added more functions to EventFactory. update my testing file

--- onsite-program/submissions/week-5/day-5/Bilal_Oyeleke_Soliu/src/TicketNFT.sol
+++ onsite-program/submissions/week-5/day-5/Bilal_Oyeleke_Soliu/src/TicketNFT.sol
@@ -7,11 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract BilalNFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
 
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        address initialOwner
-    ) ERC721(name_, symbol_) Ownable(initialOwner) {}
+    constructor(string memory name_, string memory symbol_, address initialOwner) ERC721(name_, symbol_) Ownable(initialOwner) {}
 
     function mintBilalNFT(address to, string memory tokenURI) public onlyOwner {
         _safeMint(to, nextTokenId);
--- onsite-program/submissions/week-5/day-5/Bilal_Oyeleke_Soliu/src/TicketToken.sol
+++ onsite-program/submissions/week-5/day-5/Bilal_Oyeleke_Soliu/src/TicketToken.sol
@@ -5,12 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TicketToken is ERC20, Ownable {
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint256 initialSupply,
-        address initialOwner
-    ) ERC20(name_, symbol_) Ownable(initialOwner) {
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply, address initialOwner) ERC20(name_, symbol_) Ownable(initialOwner) {
         _mint(initialOwner, initialSupply);
     }
 }
