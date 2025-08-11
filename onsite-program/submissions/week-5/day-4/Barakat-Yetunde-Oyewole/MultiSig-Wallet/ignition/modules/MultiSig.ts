1e1d71d13019161c5761d8492542cc364f2b3b7f
Labi-Joy <joyeden325@gmail.com>
2025-08-11 10:49:55 +0100

    Multisig contract deployed

---
+++ onsite-program/submissions/week-5/day-4/Barakat-Yetunde-Oyewole/MultiSig-Wallet/ignition/modules/MultiSig.ts
@@ -0,0 +1,16 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const MultisigModule = buildModule("MultisigModule", (m) => {
+  
+
+  const Multisig = m.contract("Multisig");
+
+  return { Multisig };
+});
+
+export default MultisigModule;
