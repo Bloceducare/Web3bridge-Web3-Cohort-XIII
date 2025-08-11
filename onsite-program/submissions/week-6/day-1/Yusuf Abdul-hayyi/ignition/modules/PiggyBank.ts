6e819a15467137d0cc2f14b743990a2d6b69c50f
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2025-08-12 00:02:09 +0100

    task submitted

--- onsite-program/submissions/week-6/day-1/Yusuf Abdul-hayyi/ignition/modules/PiggyBank.ts
+++ onsite-program/submissions/week-6/day-1/Yusuf Abdul-hayyi/ignition/modules/PiggyBank.ts
@@ -2,14 +2,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-  const PiggyBankModule = buildModule("PiggyBankModule", (m) => {
-  const owner = "0xe769800585E8f2dFe7AC396dd2d9201A1E95FEa6"; 
-  const token = "0x47f01eEFc47EBef78E10894CB0cAFa49df078a9e"; 
-  const lockTime = 60 * 60 * 24 * 30; 
-  const fee = 300; 
+const PiggyBankModule = buildModule("PiggyBankModule", (m) => {
+  const owner = "0xe769800585E8f2dFe7AC396dd2d9201A1E95FEa6";
+  const factoryAdmin = "0x47f01eEFc47EBef78E10894CB0cAFa49df078a9e";        // some valid address
+  const name = "PiggyBank";            
+  const token = "0x47f01eEFc47EBef78E10894CB0cAFa49df078a9e";
 
-  const piggyBank = m.contract("PiggyBank", [owner, token, lockTime, fee]);
-  
+  const piggyBank = m.contract("PiggyBank", [owner, factoryAdmin, name, token]);
+
 
   return { piggyBank };
 });
