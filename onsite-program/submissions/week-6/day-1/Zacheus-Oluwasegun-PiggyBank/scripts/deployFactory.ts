de321004b2e34ce478e1aef104f1dafc16759c73
Zacheus <segunzacheusi@gmail.com>
2025-08-11 15:42:13 +0100

    chore: factory and child contract for a piggy bank

---
+++ onsite-program/submissions/week-6/day-1/Zacheus-Oluwasegun-PiggyBank/scripts/deployFactory.ts
@@ -0,0 +1,17 @@
+import { ethers, run } from "hardhat";
+
+async function main() {
+  console.log("Deploying Piggybank factory contract to Lisk Sepolia...");
+
+  const PiggyBankFactory = await ethers.getContractFactory("PiggyBankFactory");
+  const piggybankFactory = await PiggyBankFactory.deploy();
+   await piggybankFactory.waitForDeployment();
+
+   const contractAddress = await piggybankFactory.getAddress();
+  console.log("Web3BridgeGarage contract deployed to:", contractAddress);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
