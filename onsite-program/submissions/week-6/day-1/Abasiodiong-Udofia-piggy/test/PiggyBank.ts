c863140b19ff8324d53775d38adbddcc2a2dc698
Odiong <enalsy22@gmail.com>
2025-08-12 18:41:58 +0100

    added test

--- onsite-program/submissions/week-6/day-1/Abasiodiong-Udofia-piggy/test/PiggyBank.ts
+++ onsite-program/submissions/week-6/day-1/Abasiodiong-Udofia-piggy/test/PiggyBank.ts
@@ -1,127 +1,258 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
+import { ethers } from "ethers";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("PiggyBankFactory", function () {
+  async function deployPiggyBankFactoryFixture() {
+    const [admin, user, otherUser] = await hre.ethers.getSigners();
+    const PiggyBankFactory = await hre.ethers.getContractFactory("PiggyBankFactory");
+    const factory = await PiggyBankFactory.deploy();
+    await factory.waitForDeployment();
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    const MockERC20 = await hre.ethers.getContractFactory("MockERC20");
+    const mockERC20 = await MockERC20.deploy();
+    await mockERC20.waitForDeployment();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+    return { factory, admin, user, otherUser, mockERC20 };
+  }
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  async function createEtherPiggyBankFixture() {
+    const { factory, admin, user, otherUser, mockERC20 } = await loadFixture(deployPiggyBankFactoryFixture);
+    const duration = 3600; // 1 hour
+    const tx = await factory.connect(user).createPiggyBank(duration, ethers.ZeroAddress);
+    const receipt = await tx.wait();
+    const piggyBankAddress = receipt?.logs[1].args.piggyBank;
+    const piggyBank = await hre.ethers.getContractAt("IPiggyBank", piggyBankAddress);
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { factory, piggyBank, admin, user, otherUser, mockERC20, duration };
+  }
+
+  async function createERC20PiggyBankFixture() {
+    const { factory, admin, user, otherUser, mockERC20 } = await loadFixture(deployPiggyBankFactoryFixture);
+    const duration = 3600;
+    const tx = await factory.connect(user).createPiggyBank(duration, await mockERC20.getAddress());
+    const receipt = await tx.wait();
+    const piggyBankAddress = receipt?.logs[1].args.piggyBank;
+    const piggyBank = await hre.ethers.getContractAt("IPiggyBank", piggyBankAddress);
+
+    return { factory, piggyBank, admin, user, otherUser, mockERC20, duration };
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    it("Should set the admin correctly", async function () {
+      const { factory, admin } = await loadFixture(deployPiggyBankFactoryFixture);
+      expect(await factory.admin()).to.equal(admin.address);
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    it("Should deploy correctly via Hardhat Ignition", async function () {
+      const { factory, mockERC20 } = await loadFixture(deployPiggyBankFactoryFixture);
+      const PiggyBankModule = await import("../ignition/modules/PiggyBank");
+      const { factory: ignitionFactory, mockERC20: ignitionMockERC20 } = await hre.ignition.deploy(PiggyBankModule, {
+        defaultSender: (await hre.ethers.getSigners())[0].address,
+      });
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+      expect(await ignitionFactory.admin()).to.equal(await factory.admin());
+      expect(await ignitionMockERC20.name()).to.equal(await mockERC20.name());
+      expect(await ignitionMockERC20.symbol()).to.equal(await mockERC20.symbol());
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("PiggyBank Creation", function () {
+    it("Should create a piggy bank for Ether", async function () {
+      const { factory, user } = await loadFixture(deployPiggyBankFactoryFixture);
+      const duration = 3600;
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+      await expect(factory.connect(user).createPiggyBank(duration, ethers.ZeroAddress))
+        .to.emit(factory, "PiggyBankCreated")
+        .withArgs(user.address, anyValue, (await hre.ethers.provider.getBlock("latest")).timestamp + duration, ethers.ZeroAddress);
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+      const userPiggyBanks = await factory.getUserPiggyBanksInfo(user.address);
+      expect(userPiggyBanks.length).to.equal(1);
+      expect(userPiggyBanks[0].asset).to.equal(ethers.ZeroAddress);
+      expect(userPiggyBanks[0].balance).to.equal(0);
+      expect(userPiggyBanks[0].unlockTime).to.be.closeTo(
+        (await hre.ethers.provider.getBlock("latest")).timestamp + duration,
+        5
+      );
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("Should create a piggy bank for ERC20", async function () {
+      const { factory, user, mockERC20 } = await loadFixture(deployPiggyBankFactoryFixture);
+      const duration = 3600;
 
-        await time.increaseTo(unlockTime);
+      await expect(factory.connect(user).createPiggyBank(duration, await mockERC20.getAddress()))
+        .to.emit(factory, "PiggyBankCreated")
+        .withArgs(user.address, anyValue, anyValue, await mockERC20.getAddress());
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+      const userPiggyBanks = await factory.getUserPiggyBanksInfo(user.address);
+      expect(userPiggyBanks.length).to.equal(1);
+      expect(userPiggyBanks[0].asset).to.equal(await mockERC20.getAddress());
+      expect(userPiggyBanks[0].balance).to.equal(0);
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("Should track multiple piggy banks with different lock periods", async function () {
+      const { factory, user } = await loadFixture(deployPiggyBankFactoryFixture);
+      await factory.connect(user).createPiggyBank(3600, ethers.ZeroAddress);
+      await factory.connect(user).createPiggyBank(7200, ethers.ZeroAddress);
 
-        await time.increaseTo(unlockTime);
+      const userPiggyBanks = await factory.getUserPiggyBanksInfo(user.address);
+      expect(await factory.getUserSavingsCount(user.address)).to.equal(2);
+      expect(userPiggyBanks[0].unlockTime).to.be.closeTo(
+        (await hre.ethers.provider.getBlock("latest")).timestamp + 3600,
+        5
+      );
+      expect(userPiggyBanks[1].unlockTime).to.be.closeTo(
+        (await hre.ethers.provider.getBlock("latest")).timestamp + 7200,
+        5
+      );
+    });
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+    it("Should revert if duration is zero", async function () {
+      const { factory, user } = await loadFixture(deployPiggyBankFactoryFixture);
+      await expect(factory.connect(user).createPiggyBank(0, ethers.ZeroAddress))
+        .to.be.revertedWithCustomError(factory, "ZeroDuration");
+    });
+  });
+
+  describe("Deposits and Withdrawals", function () {
+    it("Should deposit and withdraw Ether after lock period", async function () {
+      const { piggyBank, user, duration } = await loadFixture(createEtherPiggyBankFixture);
+      const depositAmount = hre.ethers.utils.parseEther("1.0");
+
+      await expect(user.sendTransaction({ to: piggyBank.address, value: depositAmount }))
+        .to.emit(piggyBank, "Deposited")
+        .withArgs(user.address, depositAmount);
+      expect(await piggyBank.getBalance()).to.equal(depositAmount);
+
+      await hre.network.provider.send("evm_increaseTime", [duration + 1]);
+      await hre.network.provider.send("evm_mine");
+
+      const initialBalance = await user.getBalance();
+      const withdrawTx = await piggyBank.connect(user).withdraw(depositAmount);
+      const withdrawReceipt = await withdrawTx.wait();
+      const gasUsed = withdrawReceipt.gasUsed.mul(withdrawReceipt.effectiveGasPrice);
+
+      expect(await piggyBank.getBalance()).to.equal(0);
+      expect(await user.getBalance()).to.be.closeTo(
+        initialBalance.add(depositAmount).sub(gasUsed),
+        hre.ethers.utils.parseEther("0.01")
+      );
+    });
+
+    it("Should deposit and withdraw ERC20 after lock period", async function () {
+      const { piggyBank, user, mockERC20, duration } = await loadFixture(createERC20PiggyBankFixture);
+      const depositAmount = hre.ethers.utils.parseEther("100");
+
+      await mockERC20.transfer(user.address, depositAmount);
+
+      await mockERC20.connect(user).approve(piggyBank.address, depositAmount);
+      await expect(piggyBank.connect(user).deposit(depositAmount))
+        .to.emit(piggyBank, "Deposited")
+        .withArgs(user.address, depositAmount);
+      expect(await piggyBank.getBalance()).to.equal(depositAmount);
+
+      await hre.network.provider.send("evm_increaseTime", [duration + 1]);
+      await hre.network.provider.send("evm_mine");
+
+      await piggyBank.connect(user).withdraw(depositAmount);
+
+      expect(await piggyBank.getBalance()).to.equal(0);
+      expect(await mockERC20.balanceOf(user.address)).to.equal(depositAmount);
+    });
+
+    it("Should charge 3% fee for early withdrawal (Ether)", async function () {
+      const { factory, piggyBank, user, admin } = await loadFixture(createEtherPiggyBankFixture);
+      const depositAmount = hre.ethers.utils.parseEther("1.0");
+      const expectedFee = depositAmount.mul(3).div(100);
+      const expectedToUser = depositAmount.sub(expectedFee);
+
+      await user.sendTransaction({ to: piggyBank.address, value: depositAmount });
+
+      const initialAdminBalance = await admin.getBalance();
+      await expect(piggyBank.connect(user).withdraw(depositAmount))
+        .to.emit(piggyBank, "Withdrawn")
+        .withArgs(user.address, expectedToUser, expectedFee);
+
+      expect(await piggyBank.getBalance()).to.equal(0);
+      expect(await admin.getBalance()).to.equal(initialAdminBalance.add(expectedFee));
+    });
+
+    it("Should charge 3% fee for early withdrawal (ERC20)", async function () {
+      const { piggyBank, user, admin, mockERC20 } = await loadFixture(createERC20PiggyBankFixture);
+      const depositAmount = hre.ethers.utils.parseEther("100");
+      const expectedFee = depositAmount.mul(3).div(100);
+      const expectedToUser = depositAmount.sub(expectedFee);
+
+      await mockERC20.transfer(user.address, depositAmount);
+
+      await mockERC20.connect(user).approve(piggyBank.address, depositAmount);
+      await piggyBank.connect(user).deposit(depositAmount);
+
+      const initialAdminBalance = await mockERC20.balanceOf(admin.address);
+      await expect(piggyBank.connect(user).withdraw(depositAmount))
+        .to.emit(piggyBank, "Withdrawn")
+        .withArgs(user.address, expectedToUser, expectedFee);
+
+      expect(await piggyBank.getBalance()).to.equal(0);
+      expect(await mockERC20.balanceOf(admin.address)).to.equal(initialAdminBalance.add(expectedFee));
+      expect(await mockERC20.balanceOf(user.address)).to.equal(expectedToUser);
+    });
+
+    it("Should revert if withdraw amount exceeds balance", async function () {
+      const { piggyBank, user } = await loadFixture(createEtherPiggyBankFixture);
+      await expect(piggyBank.connect(user).withdraw(1))
+        .to.be.revertedWithCustomError(piggyBank, "InsufficientBalance");
+    });
+
+    it("Should revert if non-owner tries to withdraw", async function () {
+      const { piggyBank, otherUser } = await loadFixture(createEtherPiggyBankFixture);
+      await expect(piggyBank.connect(otherUser).withdraw(1))
+        .to.be.revertedWithCustomError(piggyBank, "OnlyOwner");
+    });
+
+    it("Should revert if deposit amount is zero", async function () {
+      const { piggyBank, user } = await loadFixture(createEtherPiggyBankFixture);
+      await expect(piggyBank.connect(user).deposit(0))
+        .to.be.revertedWithCustomError(piggyBank, "ZeroAmount");
+    });
+
+    it("Should revert if incorrect Ether value is sent", async function () {
+      const { piggyBank, user } = await loadFixture(createEtherPiggyBankFixture);
+      await expect(piggyBank.connect(user).deposit(1, { value: 2 }))
+        .to.be.revertedWithCustomError(piggyBank, "IncorrectEtherValue");
+    });
+
+    it("Should revert if Ether is sent for ERC20 deposit", async function () {
+      const { piggyBank, user } = await loadFixture(createERC20PiggyBankFixture);
+      await expect(piggyBank.connect(user).deposit(1, { value: 1 }))
+        .to.be.revertedWithCustomError(piggyBank, "EtherSentForERC20");
+    });
+  });
+
+  describe("Queries", function () {
+    it("Should return user piggy banks info", async function () {
+      const { factory, user } = await loadFixture(deployPiggyBankFactoryFixture);
+      await factory.connect(user).createPiggyBank(3600, ethers.ZeroAddress);
+
+      const infos = await factory.getUserPiggyBanksInfo(user.address);
+      expect(infos.length).to.equal(1);
+      expect(infos[0].asset).to.equal(ethers.ZeroAddress);
+      expect(infos[0].balance).to.equal(0);
+    });
+  });
+
+  describe("Interface Compliance", function () {
+    it("Should correctly expose IPiggyBank interface functions", async function () {
+      const { piggyBank, user, admin } = await loadFixture(createEtherPiggyBankFixture);
+      expect(await piggyBank.owner()).to.equal(user.address);
+      expect(await piggyBank.admin()).to.equal(admin.address);
+      expect(await piggyBank.asset()).to.equal(ethers.ZeroAddress);
+      expect(await piggyBank.unlockTime()).to.be.closeTo(
+        (await hre.ethers.provider.getBlock("latest")).timestamp + 3600,
+        5
+      );
+      expect(await piggyBank.getBalance()).to.equal(0);
     });
   });
 });
