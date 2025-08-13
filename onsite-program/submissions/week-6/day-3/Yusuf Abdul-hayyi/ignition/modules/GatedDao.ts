e58c925086104030c04314b1c562bcfd92e25ca0
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2025-08-14 00:45:56 +0100

    task submitted

--- onsite-program/submissions/week-6/day-3/Yusuf Abdul-hayyi/ignition/modules/GatedDao.ts
+++ onsite-program/submissions/week-6/day-3/Yusuf Abdul-hayyi/ignition/modules/GatedDao.ts
@@ -5,9 +5,11 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
 const GatedDAOModule = buildModule("GatedDAOModule", (m) => {
+  const nft = "0x0fB5BFFaBc5a988283B20dBeA94867Daf28eFe93";
+  const registry = "0x1718C5eBD62bece69168877DA4f873AD1e65Aef1"
   
 
-  const gatedDao = m.contract("GatedDao");
+  const gatedDao = m.contract("GatedDAO", [nft, registry]);
 
   return { gatedDao };
 });
