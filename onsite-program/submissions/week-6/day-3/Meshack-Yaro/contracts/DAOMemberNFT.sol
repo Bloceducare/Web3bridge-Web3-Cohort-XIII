e534417d2607f0495e53dc05c3fb2c8b5029c5d2
Meshack Yaro <yaromeshack@gmail.com>
2025-08-14 15:28:43 +0100

    refactored

--- onsite-program/submissions/week-6/day-3/Meshack-Yaro/contracts/DAOMemberNFT.sol
+++ onsite-program/submissions/week-6/day-3/Meshack-Yaro/contracts/DAOMemberNFT.sol
@@ -2,12 +2,26 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DAOMemberNFT is ERC721, Ownable {
+contract DAOMemberNFT is ERC721, ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
+    string private _baseURIExtended;
 
-    constructor() ERC721("DAOMemberNFT", "DMN") Ownable(msg.sender) {}
+
+    constructor() ERC721("DAOMemberNFT", "DMN") Ownable(msg.sender) {
+        _baseURIExtended = "https://gateway.pinata.cloud/ipfs/bafkreiasoxifi73x5eisgkecpmuh74sppyvysqkdeysase5rk7i2mlsrnq";
+    }
+
+    function setBaseURI(string memory newBaseURI) external onlyOwner {
+        _baseURIExtended = newBaseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURIExtended;
+    }
+
 
     function mint(address to) external onlyOwner returns (uint256) {
         uint256 tokenId = _tokenIdCounter;
@@ -16,12 +30,16 @@ contract DAOMemberNFT is ERC721, Ownable {
         return tokenId;
     }
 
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override
-        returns (address)
-    {
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
         address previousOwner = super._update(to, tokenId, auth);
         return previousOwner;
+    }
+
+        function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
