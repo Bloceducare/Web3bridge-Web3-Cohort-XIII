87d2be8dd9237876d3789187d97748141f88f5f0
DIFoundation <adeniranibrahim165@gmail.com>
2025-08-14 06:41:17 +0100

    Deployment

--- onsite-program/submissions/week-6/day-3/Adeniran Ibrahim Adewale/ignition/modules/DAO.ts
+++ onsite-program/submissions/week-6/day-3/Adeniran Ibrahim Adewale/ignition/modules/DAO.ts
@@ -2,24 +2,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export const MockERC721Module = buildModule("MockERC721Module", (m) => {
-    const mockERC721 = m.contract("MockERC721");
-
-  return { mockERC721 };
-});
-
-// export default MockERC721Module RolesRegistryModule;
-
-export const RolesRegistryModule = buildModule("RolesRegistryModule", (m) => {
+    const mockERC721 = m.contract("MockERC721", ["RolesDAO", "RDAO"]);
     const rolesRegistry = m.contract("RolesRegistry");
+    const tokenGatedDAO = m.contract("TokenGatedDAO", ["0x6Cac76f9e8d6F55b3823D8aEADEad970a5441b67"]);
 
-  return { rolesRegistry };
+  return { mockERC721, rolesRegistry, tokenGatedDAO };
 });
 
-
-export const TokenGatedDAOModule = buildModule("TokenGatedDAOModule", (m) => {
-    const tokenGatedDAO = m.contract("TokenGatedDAO");
-
-  return { tokenGatedDAO };
-});
+export default MockERC721Module;
 
 
