aced12245f0131ded3520522342f239cb57c638c
oderahub <okere_chidera@yahoo.com>
2025-08-15 23:45:08 +0100

    chore: project

---
+++ onsite-program/submissions/week-6/day-5/chidera_okere/dynamic/ignition/modules/Unique.ts
@@ -0,0 +1,20 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+
+const ClockNFT = buildModule('ClockNFT', (m) => {
+  // Deploy the contract
+  const clockNFT = m.contract('ClockNFT')
+
+  // Your specific address as the deployer (this will be msg.sender)
+  const numberOfTokensToMint = 1
+
+  // Mint tokens - the mint() function automatically mints to msg.sender
+  for (let i = 0; i < numberOfTokensToMint; i++) {
+    m.call(clockNFT, 'mint', [], {
+      id: `mint_token_${i + 1}`
+    })
+  }
+
+  return { clockNFT }
+})
+
+export default ClockNFT
