4d234202af4faafb61092550b93422268a3e333b
jayteemoney <jethroirmiya@gmail.com>
2025-08-16 23:35:06 +0100

    adjustments

--- onsite-program/submissions/week-6/day-5/jethro-week6day5task/ignition/modules/Deploy.ts
+++ onsite-program/submissions/week-6/day-5/jethro-week6day5task/ignition/modules/Deploy.ts
@@ -1,6 +1,36 @@
-import { ethers } from "hardhat";
+// import { network } from "hardhat";
+
+// const { ethers } = await network.connect({
+//   network: "sepolia",
+//   chainType: "l1",
+// });
+// async function main(): Promise<void> {
+//   const [deployer] = await ethers.getSigners();
+//   console.log("Deploying with account:", deployer.address);
+
+//   const DynamicTimeNFT = await ethers.getContractFactory("DynamicTimeNFT");
+//   const nft = await DynamicTimeNFT.deploy();
+//   await nft.waitForDeployment();
+//   const contractAddress = await nft.getAddress();
+//   console.log("DynamicTimeNFT deployed to:", contractAddress);
+
+//   // Mint a test NFT
+//   const tx = await nft.mint(deployer.address);
+//   await tx.wait();
+//   console.log("Minted NFT ID 1 to:", deployer.address);
+// }
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+import { network } from "hardhat";
 
 async function main(): Promise<void> {
+  const { ethers } = await network.connect({
+    network: "sepolia",
+    chainType: "l1",
+  });
   const [deployer] = await ethers.getSigners();
   console.log("Deploying with account:", deployer.address);
 
