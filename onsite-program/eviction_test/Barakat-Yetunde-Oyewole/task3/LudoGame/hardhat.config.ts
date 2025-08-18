9fffa69a5affe6807dd24d0963ec481ca2408485
Oyetundee <ayeti78@gmail.com>
2025-08-18 18:33:25 +0100

    update

--- onsite-program/eviction_test/Barakat-Yetunde-Oyewole/task3/LudoGame/hardhat.config.ts
+++ onsite-program/eviction_test/Barakat-Yetunde-Oyewole/task3/LudoGame/hardhat.config.ts
@@ -1,8 +1,36 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    liskSepolia: {
+      url: process.env.LISK_SEPOLIA_URL || "https://rpc.sepolia-api.lisk.com",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 4202,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      liskSepolia: process.env.ETHERSCAN_API_KEY || "123",
+    },
+    customChains: [
+      {
+        network: "liskSepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
