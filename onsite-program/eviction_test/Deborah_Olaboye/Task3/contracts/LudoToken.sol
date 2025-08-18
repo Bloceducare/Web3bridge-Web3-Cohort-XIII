2918554c7b15788cb62b7efa7866af61c319ee83
Deborah Olaboye <deboraholaboye@gmail.com>
2025-08-18 19:56:34 +0100

    task 3 updated

--- onsite-program/eviction_test/Deborah_Olaboye/Task3/contracts/LudoToken.sol
+++ onsite-program/eviction_test/Deborah_Olaboye/Task3/contracts/LudoToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LudoToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Ludo", "LUDO") {
-        _mint(msg.sender, initialSupply); // Sends the initial tokens to whoever deploys it (you!)
+        _mint(msg.sender, initialSupply);
     }
 }
