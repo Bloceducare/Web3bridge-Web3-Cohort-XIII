11a180e6aa73b83fe736d0bdd418d61626b0d511
Ryjen1 <ryjenspark@gmail.com>
2025-08-18 20:11:16 +0100

    task-3

--- onsite-program/eviction_test/Maryjane/Task-3/hardhat.config.ts
+++ onsite-program/eviction_test/Maryjane/Task-3/hardhat.config.ts
@@ -1,17 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
-if (!process.env.PRIVATE_KEY) {
-  throw new Error("Please set your PRIVATE_KEY in a .env file");
-}
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.26",
+    version: "0.8.28",
     settings: {
       optimizer: {
         enabled: true,
@@ -20,29 +12,13 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    "lisk-sepolia": {
-      url: "https://rpc.sepolia-api.lisk.com",
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 1_000_000_000,
+    hardhat: {
+      chainId: 1337,
     },
   },
-  etherscan: {
-    apiKey: {
-      "lisk-sepolia": "unused",
-    },
-    customChains: [
-      {
-        network: "lisk-sepolia",
-        chainId: 4202,
-        urls: {
-          apiURL: "https://sepolia-blockscout.lisk.com/api",
-          browserURL: "https://sepolia-blockscout.lisk.com",
-        },
-      },
-    ],
-  },
-  sourcify: {
-    enabled: false,
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
   },
 };
 
