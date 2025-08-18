be7bd06f744a34a94c36f536409893903d586974
DIFoundation <adeniranibrahim165@gmail.com>
2025-08-18 16:31:44 +0100

    #

--- onsite-program/submissions/week-6/day-5/Adeniran Ibrahim Adewale/contracts/OnChainClock.sol
+++ onsite-program/submissions/week-6/day-5/Adeniran Ibrahim Adewale/contracts/OnChainClock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
