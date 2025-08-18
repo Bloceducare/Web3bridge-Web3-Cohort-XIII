11e14e930a6f7fc373b941252e88de028747e32b
IyanuOluwaJesuloba <jesulobaowoseni1@gmail.com>
2025-08-18 19:59:37 +0100

    .

--- onsite-program/eviction_test/Owoseni/Task3/test/Ludo.ts
+++ onsite-program/eviction_test/Owoseni/Task3/test/Ludo.ts
@@ -11,18 +11,18 @@ describe("LudoGame", function () {
   beforeEach(async function () {
     const Token = await ethers.getContractFactory("ERC20");
     token = await Token.deploy("TestToken", "TST") as ERC20;
-    await token.deployed();
+    await token.waitForDeployment();
 
     const LudoGame = await ethers.getContractFactory("LudoGame");
     ludo = await LudoGame.deploy(token.address) as LudoGame;
-    await ludo.deployed();
+    await ludo.waitForDeployment();
 
     signers = await ethers.getSigners();
     // Mint tokens to players
-    await token.mint(signers[0].address, ethers.utils.parseEther("100"));
-    await token.mint(signers[1].address, ethers.utils.parseEther("100"));
-    await token.approve(ludo.address, ethers.utils.parseEther("100"));
-    await token.connect(signers[1]).approve(ludo.address, ethers.utils.parseEther("100"));
+    await token.mint(signers[0].address, parseEther("100"));
+    await token.mint(signers[1].address, parseEther("100"));
+    await token.approve(ludo.address, parseEther("100"));
+    await token.connect(signers[1]).approve(ludo.address, parseEther("100"));
   });
 
   it("Should register players", async function () {
@@ -33,7 +33,7 @@ describe("LudoGame", function () {
 
   it("Should allow staking and moving", async function () {
     await ludo.register("Player1", 0);
-    await token.approve(ludo.address, ethers.utils.parseEther("100"));
+    await token.approve(ludo.address, parseEther("100"));
     await ludo.stakeTokens();
     await ludo.makeMove();
     const [name, color, position, stakedTokens] = await ludo.getPlayer(signers[0].address);
@@ -48,10 +48,10 @@ describe("LudoGame", function () {
 
     // Simulate reaching end (set position manually for testing)
     await ludo.makeMove(); // Move once
-    await ethers.provider.send("evm_increaseTime", [3600]); // Move time for randomness
+    await ethers.provider.network.provider.send("evm_increaseTime", [3600]); // Move time for randomness
     await ludo.makeMove(); // Move again to reach end
     const winner = await ludo.winner();
     expect(winner).to.equal(signers[0].address);
-    expect(await token.balanceOf(signers[0].address)).to.equal(ethers.utils.parseEther("100"));
+    expect(await token.balanceOf(signers[0].address)).to.equal(parseEther("100"));
   });
 });
